7eb2d9fe6a2bb4bf6dc281b5de26e15a2d6bd5a0
Jayanth <jayanthjaiswal15@gmail.com>
2018-05-04 11:08:18 -0700

    more fixes.

--- cdli_mpa_tool/checker.py
+++ cdli_mpa_tool/checker.py
@@ -26,7 +26,7 @@ class CONLChecker:
         with codecs.open(self.inputFileName, 'r', 'utf-8') as openedFile:
             total_lines = 0
             for (i, line) in enumerate(openedFile):
-                self.__parse(i, line.strip())
+                self.__parse(i+1, line.strip())
                 total_lines += 1
             if total_lines < 2:
                 click.echo(
@@ -51,7 +51,7 @@ class CONLChecker:
         count_dot = xpostag.count('.')
         if count_dot != count_dash:
             click.echo(
-                '\nError: The xpostag dot count {0} in line number {1} in file {2} does not match with segm dash count {3}.'
+                '\nWarning: The xpostag dot count {0} in line number {1} in file {2} does not match with segm dash count {3}.'
                     .format(count_dot, linenumber, self.inputFileName, count_dash))
             self.iscorrect = False
         xpostag_pattern = u"^(([A-Z0-9]+)(\-([A-Z0-9]+))*)(\.([A-Z0-9]+)(\-([A-Z0-9]+))*)+|[A-Z]+$"
@@ -78,7 +78,7 @@ class CONLChecker:
         elif postag_pos != 0 and (
                 xpostag_splitted[postag_pos] not in SPECIAL_POSTAGS):
             click.echo(
-                '\nError: The xpostag {0} in line number {1} in file {2} has a prefix without {3} not being verb postag.'
+                '\nError: The xpostag {0} in line number {1} in file {2} has a prefix but {3} is not a verb postag.'
                     .format(xpostag, linenumber, self.inputFileName,
                             xpostag_splitted[postag_pos]))
             self.iscorrect = False
@@ -92,26 +92,26 @@ class CONLChecker:
                 for p in prefix_list:
                     if p not in VERB_PREFIX:
                         click.echo(
-                            '\nError: The xpostag {0} in line number {1} in file {2} has a prefix not in verb postag prefix list {3}.'
+                            u'\nError: The xpostag {0} in line number {1} in file {2} has a prefix {3} not in verb postag prefix list {4}.'
                                 .format(xpostag, linenumber,
-                                        self.inputFileName,
+                                        self.inputFileName, p,
                                         VERB_PREFIX))
                         self.iscorrect = False
                 for s in suffix_list:
                     if s not in VERB_SUFFIX:
                         click.echo(
-                            '\nError: The xpostag {0} in line number {1} in file {2} has a suffix not in verb postag suffix list {3}.'
+                            u'\nError: The xpostag {0} in line number {1} in file {2} has a suffix {3} not in verb postag suffix list {4}.'
                                 .format(xpostag, linenumber,
-                                        self.inputFileName,
+                                        self.inputFileName, s,
                                         VERB_SUFFIX))
                         self.iscorrect = False
             else:
                 for s in suffix_list:
                     if s not in NOUN_MORPHS:
                         click.echo(
-                            '\nError: The xpostag {0} in line number {1} in file {2} has a suffix not in noun postag suffix list {3}.'
+                            u'\nError: The xpostag {0} in line number {1} in file {2} has a suffix {3} not in noun postag suffix list {4}.'
                                 .format(xpostag, linenumber,
-                                        self.inputFileName,
+                                        self.inputFileName, s,
                                         NOUN_MORPHS))
                         self.iscorrect = False
         # check segm
@@ -119,7 +119,7 @@ class CONLChecker:
         # check xpostag
         if not re.compile(segm_pattern).match(segm):
             click.echo(
-                '\nError: The segm {0} in line number {1} in file {2} does not follow the format "{3}".'
+                u'\nError: The segm {0} in line number {1} in file {2} does not follow the format "{3}".'
                     .format(segm, linenumber, self.inputFileName,
                             segm_pattern))
             self.iscorrect = False
@@ -128,7 +128,7 @@ class CONLChecker:
         line_splitted = line.split('\t')
         if len(line_splitted) < 4:
             click.echo(
-                "\nError: No field should be empty in file {0} linenumber {1} (An underscore should be added in fields where we can't add something)."
+                u"\nError: No field should be empty in file {0} linenumber {1} (An underscore should be added in fields where we can't add something)."
                     .format(self.inputFileName, linenumber))
             self.iscorrect = False
         else:
@@ -140,7 +140,7 @@ class CONLChecker:
             id_pattern = r"^e?(o|r|t|b|l|ri|(s?[a-z]?[0-9]?))\.((col|b)[0-9]'?\.)?[0-9]+'?\.[0-9]+$"
             if not re.compile(id_pattern).match(id_form):
                 click.echo(
-                    '\nError: The id {0} in line number {1} in file {2} does not follow the Conll format "{3}".'
+                    u'\nError: The id {0} in line number {1} in file {2} does not follow the Conll format "{3}".'
                         .format(id_form, linenumber, self.inputFileName,
                                 id_pattern))
                 self.iscorrect = False
@@ -149,17 +149,17 @@ class CONLChecker:
     def __parse(self, linenumber, line):
         p_pattern = r"^#new_text=[a-zA-Z0-9]+\s*$"
         heading_pattern = r"#\s+ID\s+FORM\s+SEGM\s+XPOSTAG\s+HEAD\s+DEPREL\s+MISC\s*"
-        if linenumber == 0 and not re.compile(p_pattern).match(line):
+        if linenumber == 1 and not re.compile(p_pattern).match(line):
             click.echo(
                 '\nError: The line number {0} in file {1} does not follow the Conll format "{2}".'
                     .format(linenumber, self.inputFileName, p_pattern))
             self.iscorrect = False
-        elif linenumber == 1 and not re.compile(heading_pattern).match(line):
+        elif linenumber == 2 and not re.compile(heading_pattern).match(line):
             click.echo(
                 '\nError: The line number {0} in file {1} does not follow the Conll format "{2}".'
                     .format(linenumber, self.inputFileName, heading_pattern))
             self.iscorrect = False
-        elif linenumber > 1:
+        elif linenumber > 2:
             self.check_line(linenumber, line)
         else:
             pass
